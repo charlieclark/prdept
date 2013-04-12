b1c1832ddecedc039364bce65287e82d9e1d5153
Charlie Clark <charlie@stinkdigital.com>
2013-04-12 12:37:54 -0400

    new assets

--- assets/js/assetData.js
+++ assets/js/assetData.js
@@ -18,7 +18,7 @@ var imageGroups = {
 	{
 		"array":
 		[
-			{ name: "splash-bg", url : "splash-bg.jpg" }
+			{ name: "splash-bg", url : "bgs/splash-bg.jpg" }
 		]
 		
 	},
@@ -27,14 +27,14 @@ var imageGroups = {
 	{
 		"array":
 		[
-			{ name: "philosophy1-bg", url : "philosophy1-bg.jpg" },
-			{ name: "philosophy2-bg", url : "philosophy2-bg.jpg" },
-			{ name: "philosophy3-bg", url : "philosophy3-bg.jpg" },
-			{ name: "philosophy4-bg", url : "philosophy4-bg.jpg" },
-			{ name: "services-bg", url : "services-bg.jpg" },
-			{ name: "clients-bg", url : "clients-bg.jpg" },
-			{ name: "team-bg", url : "team-bg.jpg" },
-			{ name: "contact-bg", url : "contacts-bg.jpg" }
+			{ name: "philosophy1-bg", url : "bgs/philosophy1-bg.jpg" },
+			{ name: "philosophy2-bg", url : "bgs/philosophy2-bg.jpg" },
+			{ name: "philosophy3-bg", url : "bgs/philosophy3-bg.jpg" },
+			{ name: "philosophy4-bg", url : "bgs/philosophy4-bg.jpg" },
+			{ name: "services-bg", url : "bgs/services-bg.jpg" },
+			{ name: "clients-bg", url : "bgs/clients-bg.jpg" },
+			{ name: "team-bg", url : "bgs/team-bg.jpg" },
+			{ name: "contact-bg", url : "bgs/contacts-bg.jpg" }
 		]
 	}
 	
